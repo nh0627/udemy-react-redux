017f48e6f2bf761407c04709361fda13de295b96
Nahyeon Lee <naboee0627@gmail.com>
2018-12-28 20:01:51 +0900

    Update comment

--- 14.blog/src/actions/index.js
+++ 14.blog/src/actions/index.js
@@ -9,7 +9,8 @@ export const fetchPostsAndUsers = () => async (dispatch, getState) => {
         .map('userId')
         .uniq()
         .forEach(id => dispatch(fetchUser(id)))
-        .value();
+        .value(); // Execute all the steps!
+
     // const userIds = _.uniq(_.map(getState().posts, 'userId'));
     // userIds.forEach(id => dispatch(fetchUser(id)));
 };
@@ -22,13 +23,11 @@ export const fetchPosts = () => async dispatch => {
 
 export const fetchUser = id => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
-
     dispatch({ type: 'FETCH_USER', payload: response.data });
 };
 
 // export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
 // const _fetchUser = _.memoize( async (id, dispatch) => {
 //     const response = await jsonPlaceholder.get(`/users/${id}`);
-
 //     dispatch({ type: 'FETCH_USER', payload: response.data });
 // });
