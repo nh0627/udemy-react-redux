658a643a67436a7a56df55be36cbb37dbda6c3b3
Nahyeon Lee <naboee0627@gmail.com>
2018-12-30 22:26:49 +0900

    Add editing stream feature

--- 16.streams/client/src/actions/index.js
+++ 16.streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/axios/streams';
+import history from '../history';
 import { 
     SIGN_IN, 
     SIGN_OUT, 
@@ -27,6 +28,8 @@ export const createStream = formValues => async (dispatch, getState) => {
     const response = await streams.post('/streams', { ...formValues, userId });
 
     dispatch({type: CREATE_STREAM, payload: response.data });
+    // Do some programmatic navigation to forcibly get a user back to the root route
+    history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
@@ -42,9 +45,10 @@ export const fetchStream = id => async dispatch => {
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-    const response = await streams.put(`streams/${id}`, formValues);
-
+    // const response = await streams.put(`streams/${id}`, formValues); Update all properties of record
+    const response = await streams.patch(`streams/${id}`, formValues); // Update some properties of record that needs to be updated
     dispatch({type: EDIT_STREAM, payload: response.data });
+    history.push('/');
 }
 
 export const deleteStream = id => async dispatch => {
--- 16.streams/client/src/components/App.js
+++ 16.streams/client/src/components/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import StreamCreate from '../components/streams/StreamCreate';
 import StreamList from '../components/streams/StreamList';
 import StreamEdit from '../components/streams/StreamEdit';
 import StreamShow from '../components/streams/StreamShow';
 import StreamDelete from '../components/streams/StreamDelete';
 import Header from './Header';
+import history from '../history';
 
 const App = () => {
     return (
         <div className="ui container">
-            <BrowserRouter>
+            <Router history={history}>
                 <div>
                 <Header />
                     <Route path="/" exact component={StreamList} />
                     <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit" exact component={StreamEdit} />
-                    <Route path="/streams/delete" exact component={StreamDelete} />
-                    <Route path="/streams/show" exact component={StreamShow} />
+                    <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                    <Route path="/streams/delete/:id" exact component={StreamDelete} />
+                    <Route path="/streams/show/:id" exact component={StreamShow} />
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     );
 }
